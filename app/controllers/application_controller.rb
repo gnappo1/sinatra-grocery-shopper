020917c20295680d3afa1e658bec7ab7b3f6a729
Matteo Piccini <gnappo.1@live.it>
2017-06-22 10:00:08 +0000

    added helpers to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,25 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :welcome
   end
-  
+
+  helpers do
+    def current_shopper
+      @current_shopper ||= Shopper.find_by(id: session[:id])
+    end
+
+    def current_client
+      @current_client ||= Client.find_by(id: session[:id])
+    end
+
+    def logged_in?
+      !!session[:id]
+    end
+
+    def not_logged_in?
+      if !logged_in?
+        redirect('/')
+      end
+    end
+  end
+
 end
